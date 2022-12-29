2d171a9d97a57e5c16692cc0bdd4b210b39a3fce
Jorge Ramirez <systemgds@gmail.com>
2022-12-28 23:24:08 -0500

    Actualizar mi Ejercicio de Volales

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -233,16 +233,10 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if( letra == "a"){
+  if( letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"  ){
     return "Es vocal"
-  } else if(letra == "e"){
-    return "Es vocal"
-  } else if(letra == "i"){
-  return "Es vocal"
-  } else if(letra == "o"){
-  return "Es vocal"
-  } else if(letra == "u"){
-  return "Es vocal"
+   } else if(letra.length > 1){
+     return "Dato incorrecto"
   }
   return "Dato incorrecto"
 }
